ec63cf2948a02a2990b008d796d86eeed69ea653
Can Zunal <98685681+CZunal@users.noreply.github.com>
2024-05-06 21:40:19 +0300

    Dummy navbar added

--- committees_management_system/src/App.js
+++ committees_management_system/src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
