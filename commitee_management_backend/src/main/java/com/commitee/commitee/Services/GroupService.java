e6293009d4976b77f110f3a9c10d0303e52f1320
Can Zunal <98685681+CZunal@users.noreply.github.com>
2024-05-10 12:52:17 +0300

    Update Group Backend

---
+++ commitee_management_backend/src/main/java/com/commitee/commitee/Services/GroupService.java
@@ -0,0 +1,28 @@
+package com.commitee.commitee.Services;
+
+import com.commitee.commitee.Entities.Group;
+import com.commitee.commitee.Repositories.GroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GroupService {
+    private final GroupRepository groupRepository;
+
+    @Autowired
+    public GroupService(GroupRepository groupRepository) {
+        this.groupRepository = groupRepository;
+    }
+
+    public List<Group> getAllGroups() {
+        return groupRepository.findAll();
+    }
+
+    public Group saveGroup(Group group) {
+        return groupRepository.save(group);
+    }
+
+    // Other methods for retrieving, updating, and deleting groups can be added here
+}
